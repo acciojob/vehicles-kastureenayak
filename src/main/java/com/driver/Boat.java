f8a60a04487ccfbb58b3cfac391dee487e20dcd9
Kasturee Nayak <kasturee1998@gmail.com>
2024-04-25 20:22:34 +0530

    Vehicle

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+}
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -4,19 +4,47 @@ public class Vehicle {
 
     private String name;
     private int currentSpeed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
     private int currentDirection;
 
     public Vehicle(String name) {
-
+      setName(name);
     }
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
+        currentDirection=direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
+        currentSpeed=speed;
+        currentDirection=direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
